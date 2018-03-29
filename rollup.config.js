ff0824b5ad68e0396c9868fb12b10d26ba7803b0
atomita <sleeping.cait.sith+gh@gmail.com>
2018-03-29 15:59:48 +0900

    first

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/deep-set.ts',
+  output: {
+    file: 'dist/deep-set.js',
+    format: 'umd',
+    name: 'flattenObject',
+    sourceMap: true,
+  },
+  plugins: [typescript()],
+}
