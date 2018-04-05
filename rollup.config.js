1bee9f4d0aba6d0295b24326684ddba0572787a4
atomita <sleeping.cait.sith+gh@gmail.com>
2018-04-05 14:52:12 +0900

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
   output: {
     file: 'dist/deep-set.js',
     format: 'umd',
-    name: 'optimizatedDeepSet',
+    name: 'deepSet',
     sourceMap: true,
   },
   plugins: [typescript()],
